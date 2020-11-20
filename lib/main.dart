782f775f01557ce346c6f84e4ee574eef3cdfb83
Lee Chun Yit <chunyitlee@gmail.com>
2020-11-20 18:21:15 +0800

    added dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'pages/search_screen.dart';
 import 'pages/profile_page.dart';
 import 'pages/activity_history.dart';
 import 'pages/home_page.dart';
-//import test
 
 void main() {
   runApp(new MaterialApp(
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,12 +3,44 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-        child: new Center(
-      child: new Text(
-        'HOme Here!  ',
-        style: new TextStyle(fontSize: 20.0),
-      ),
-    ));
+    return new Container(child: new Center(
+        child: ListView.builder(itemBuilder: (BuildContext context, int index) {
+      return new Card(
+          child: const ListTile(
+        leading: const Icon(Icons.album),
+        title: const Text('The Enchanted Nightingale'),
+        subtitle: const Text(
+          'Music by Julie Gable. Lyrics by Sidney Stein.',
+        ),
+      ));
+    })));
   }
 }
+
+// Card(
+//           child: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             children: <Widget>[
+//               const ListTile(
+//                 leading: Icon(Icons.album),
+//                 title: Text('The Enchanted Nightingale'),
+//                 subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+//               ),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.end,
+//                 children: <Widget>[
+//                   TextButton(
+//                     child: const Text('BUY TICKETS'),
+//                     onPressed: () {/* ... */},
+//                   ),
+//                   const SizedBox(width: 8),
+//                   TextButton(
+//                     child: const Text('LISTEN'),
+//                     onPressed: () {/* ... */},
+//                   ),
+//                   const SizedBox(width: 8),
+//                 ],
+//               ),
+//             ],
+//           ),
+//         )
