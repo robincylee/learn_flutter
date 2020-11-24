05c7d1bc1595b0b4558fca67a2509603eb1e674e
Lee Chun Yit <chunyitlee@gmail.com>
2020-11-24 14:17:40 +0800

    complete home_page template

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
 
+enum IconDisplay { star_border, star_half, star }
+
 class HomePage extends StatelessWidget {
+  ScrollController _controller =
+      ScrollController(initialScrollOffset: 100.0 * 5);
   @override
   Widget build(BuildContext context) {
-    return new Container(child: new Center(
-        child: ListView.builder(itemBuilder: (BuildContext context, int index) {
-      return new Card(
-          child: const ListTile(
-        leading: const Icon(Icons.album),
-        title: const Text('The Enchanted Nightingale'),
-        subtitle: const Text(
-          'Music by Julie Gable. Lyrics by Sidney Stein.',
-        ),
-      ));
-    })));
+    return new Container(
+        child: new Center(
+            child: ListView.builder(
+                controller: _controller,
+                itemCount: 20,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 100,
+                    child: Card(
+                        child: ListTile(
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                      onTap: () {
+                        print("Develop navigator pushreplacement $index");
+                      },
+                      leading: getMethod(index),
+                      title: Text('The Enchanted Nightingale'),
+                      subtitle: Text(
+                          'Music by Julie Gable. Lyrics by Sidney Stein. Music by Julie Gable. Lyrics by Sidney Stein. Music by Julie Gable. Lyrics by Sidney Stein. Music by Julie Gable. Lyrics by Sidney Stein. Music by Julie Gable. Lyrics by Sidney Stein. Music by Julie Gable. Lyrics by Sidney Stein.',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis),
+                    )),
+                  );
+                })));
+  }
+}
+
+getMethod(value) {
+  if (value < 3) {
+    return Icon(Icons.star);
+  } else if (value < 6) {
+    return Icon(Icons.star_half);
+  } else {
+    return Icon(Icons.star_border);
   }
 }
 
